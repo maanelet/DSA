039ef4920b4a8ea9237afc7934185dcc49e856ae
maanelet <165885981+maanelet@users.noreply.github.com>
2024-05-13 09:43:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let mapContainer = document.getElementById("map-container");
+let buttonsContainer = document.getElementById("buttons-container");
+let backButton = document.getElementById("back-button");
+
+function showMap(location) {
+  buttonsContainer.style.display = "none";
+  backButton.style.display = "block";
+  switch (location) {
+    case "mecanicos":
+      mapContainer.innerHTML = "<img src='mecanicos-map.jpg' alt='Mapa de Mecánicos'>";
+      break;
+    case "hospitales":
+      mapContainer.innerHTML = "<img src='hospitales-map.jpg' alt='Mapa de Hospitales'>";
+      break;
+    case "comisarias":
+      mapContainer.innerHTML = "<img src='comisarias-map.jpg' alt='Mapa de Comisarías'>";
+      break;
+    default:
+      mapContainer.innerHTML = "<p>No se encontró el mapa para esta ubicación.</p>";
+  }
+}
+
